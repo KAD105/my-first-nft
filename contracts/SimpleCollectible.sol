2264354e0f6c7132bc858c9211c1b420f159848f
KAD105 <kazem_abdi@yahoo.com>
2022-05-29 11:10:21 +0200

    Initial commit

---
+++ contracts/SimpleCollectible.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.6;
+
+
+import "@OpenZeppelin/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+
+contract SimpleCollectible is ERC721{
+    
+}
