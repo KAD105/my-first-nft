ebcb2784f957baefe059f24e9dc5d148231c0842
KAD105 <kazem_abdi@yahoo.com>
2022-06-25 19:51:36 +0200

    The nft is being displayed on opensea.testnet

--- scripts/deploy_nft.py
+++ scripts/deploy_nft.py
@@ -2,6 +2,11 @@ from scripts.helpful_scripts import get_account
 from brownie import ShapeNft
 
 
+sample_token_uri = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
+
+
 def main():
     account = get_account()
     shape_nft = ShapeNft.deploy({"from": account})
+    tx = shape_nft.createShapeNft(sample_token_uri, {"from": account})
+    tx.wait(1)
